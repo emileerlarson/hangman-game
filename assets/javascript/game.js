e10f39e6457f01b26b136e1ff751f629c5cd32e7
emileerlarson <emileerlarson@gmail.com>
2017-10-03 18:03:45 -0600

    octthree

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
-var wordList = ["pumpkin", "ghost", "bat", "trick or treat", "candy", "whitch",
+var wordList = ["pumpkin", "ghost", "bat", "trick or treat", "candy", "whitch", "black cat",
 	"spider", "clown", "fairy", "cider", "grave", "zombie", "frankinstine", "vampire", "jack o lantern", "skeloton"];
-var numberWins = document.getElementsByTagName('wins');
-var numberlosses = document.getElementsByTagName('loss');
+
 //computer picks word
 var computerChoice = wordList[Math.floor(Math.random() * wordList.length)];
 
@@ -18,20 +17,27 @@ var wins;
 // document.onkeyup = function(event) {
 
 // var userGuess = event.key;
-
+ var letterGuess = [];
 function lettersGuessed (){
 document.onkeyup = function(event) {
-
+// console.log("fire")	// var letterGuess = [];
 	var userGuess = event.key;
+	//console.log(userGuess);
 		if (remaningLetters > 0){
 			for (var j = 0; j < computerChoice.length; j++){
 				if (computerChoice[j] === userGuess){
 					answerArray[j] = userGuess;
+					document.getElementById("computerChoice").innerHTML = answerArray;
+					document.getElementById("computerChoice").innerHTML = answerArray.join(" ");
 					remaningLetters--;
 				}
 				else if (computerChoice[j] !== userGuess){
+					letterGuess.push(userGuess);
+					document.getElementById("letterGuess").innerHTML = letterGuess;
 					--rGuess;
-					document.getElementById("letterGuess").innerHTML = userGuess;	
+				}
+				else{
+					break;
 				}
 			}
 		}
@@ -43,13 +49,14 @@ document.onkeyup = function(event) {
 		}
 }
 }
-
+lettersGuessed();
+// document.getElementById("userGuess").innerHTML = (letterGuess[j]);
 document.getElementById("wins").innerHTML = wins;
 document.getElementById("loss").innerHTML = loss;
 document.getElementById("rGuess").innerHTML = rGuess;
-document.getElementById("computerChoice").innerHTML = answerArray;
+// document.getElementById("computerChoice").innerHTML = answerArray;
 document.getElementById("computerChoice").innerHTML = answerArray.join(" ");
-lettersGuessed();
+// lettersGuessed();
 
 
 
