dd3b62b06e32bcfe90c9f1b8831a00f47d1c2261
emileerlarson <emileerlarson@gmail.com>
2017-10-05 17:47:36 -0600

    octfive

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,39 @@
 var wordList = ["pumpkin", "ghost", "bat", "trickortreat", "candy", "whitch", "blackcat",
 	"spider", "clown", "fairy", "cider", "grave", "zombie", "frankinstine", "vampire", "jackolantern", "skeloton"];
+var loss = 0;
+var wins = 0;
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("loss").innerHTML = loss;
+var rGuess = 10;
+document.getElementById("rGuess").innerHTML = rGuess;
 
-//computer picks word
+
+function resetGame(){
+	rGuess = 10;
+	document.getElementById("rGuess").innerHTML = rGuess;
+	letterGuess = [];
+	document.getElementById('letterGuess').innerHTML = letterGuess;
+	startGame();
+}
+
+function startGame(){
 var computerChoice = wordList[Math.floor(Math.random() * wordList.length)];
 
 var answerArray = [];
 for (var i = 0; i < computerChoice.length; i++){
-	answerArray[i] = "_";
-}
+	answerArray[i] = "_";}
+document.getElementById("computerChoice").innerHTML = answerArray.join(" ");
 
 var remaningLetters = answerArray.length;
-
 var rGuess = 10;
-var loss = 0;
-var wins = 0;
+
+
 
 var letterGuess = [];
 document.onkeyup = function(event) {
 
 	var userGuess = event.key;
-	
+
 		if (answerArray.join('').toString() != computerChoice && rGuess !== 0){
 			
 				if (computerChoice.indexOf(userGuess) !== -1){
@@ -44,17 +58,17 @@ document.onkeyup = function(event) {
 		else if (rGuess <= 0){
 			loss++;
 			document.getElementById("loss").innerHTML = loss;
+			resetGame();
 		}
 		else{
 			wins++;
 			document.getElementById("wins").innerHTML = wins;
+			resetGame();
 		}
-}
+}}
+startGame();
 
 
-document.getElementById("wins").innerHTML = wins;
-document.getElementById("loss").innerHTML = loss;
-document.getElementById("computerChoice").innerHTML = answerArray.join(" ");
 
 
 
